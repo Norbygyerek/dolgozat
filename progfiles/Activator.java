16650df1095136044221eb8a7a0a1875ee38703c
Gáll Norbert <gnim1216@scs.ubbcluj.ro>
2015-05-15 00:19:53 +0300

    Osgi LifeCycle kész

---
+++ progfiles/Activator.java
@@ -0,0 +1,12 @@
+public class Activator implements BundleActivator  {
+	
+    public void start(BundleContext context) throws Exception {
+    	
+    	// ....
+    }
+    
+    public void stop(BundleContext context) throws Exception {
+    	
+    	// ....
+    }   
+}
